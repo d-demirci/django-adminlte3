e465038e3fdee916b443cb89f7566c314bd2b6b6
Adam Charnock <adam@adamcharnock.com>
2016-11-25 10:40:13 +0000

    Initial work on importing django_adminlte_x by @dnaextrim

---
+++ django_adminlte_theme/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoAdminlteThemeConfig(AppConfig):
+    name = 'django_adminlte_theme'
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+setup(
+    name='django-adminlte2',
+    version=open('VERSION').read().strip(),
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/adamcharnock/django-adminlte2',
+    license='MIT',
+    description='Admin LTE templates, admin theme, and template tags for Django',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    # Any requirements here, e.g. "Django >= 1.1.1"
+    install_requires=[
+        'django',
+    ],
+)
