8c20bccec50e483294aecdec4eca5d157c5a394d
Adam Charnock <adam@adamcharnock.com>
2016-12-12 22:25:20 +0000

    Work on packaging of static files

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     license='MIT',
     description='Admin LTE templates, admin theme, and template tags for Django',
     long_description=open('README.rst').read() if exists("README.rst") else "",
+    include_package_data=True,
     # Any requirements here, e.g. "Django >= 1.1.1"
     install_requires=[
         'django',
