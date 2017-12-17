4d04d5403b35cc1d4b598de178010ae84415fb3b
Adam Charnock <adam@adamcharnock.com>
2017-12-17 18:51:07 +0000

    Fixing django 2.0 compatability (closes #11, #12, #13)

---
+++ django_adminlte/compat.py
@@ -0,0 +1,8 @@
+import django
+
+
+def is_authenticated(user):
+    if django.VERSION >= (2, 0):
+        return user.is_authenticated()
+    else:
+        return user.is_authenticated
--- django_adminlte/templatetags/adminlte_helpers.py
+++ django_adminlte/templatetags/adminlte_helpers.py
@@ -4,6 +4,8 @@ from django import template
 from django.conf import settings
 from django.urls import reverse
 
+from django_adminlte.compat import is_authenticated
+
 register = template.Library()
 
 
@@ -17,7 +19,7 @@ def avatar_url(context, size=None, user=None):
     # TODO: Make behaviour configurable
     user = context['request'].user if user is None else user
     return 'https://www.gravatar.com/avatar/{hash}?s={size}&d=mm'.format(
-        hash=md5(user.email.encode('utf-8')).hexdigest() if user.is_authenticated() else '',
+        hash=md5(user.email.encode('utf-8')).hexdigest() if is_authenticated(user) else '',
         size=size or '',
     )
 
