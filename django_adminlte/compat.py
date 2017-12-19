b0695fd68806feb6d68e676fce1e873ec5216730
basitsattar <basit850@gmail.com>
2017-12-20 01:18:32 +0500

    Solved Bool object isn't callable issue

--- django_adminlte/compat.py
+++ django_adminlte/compat.py
@@ -3,6 +3,6 @@ import django
 
 def is_authenticated(user):
     if django.VERSION >= (2, 0):
-        return user.is_authenticated()
-    else:
         return user.is_authenticated
+    else:
+        return user.is_authenticated()
