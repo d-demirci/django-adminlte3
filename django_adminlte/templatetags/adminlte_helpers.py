92649eae85d1ea6c30e2a477b0576ff9be9f90a2
Adam Charnock <adam@adamcharnock.com>
2018-07-02 11:42:33 +0100

    Adding 'not_when' parameter to add_active template tag

--- django_adminlte/templatetags/adminlte_helpers.py
+++ django_adminlte/templatetags/adminlte_helpers.py
@@ -27,11 +27,17 @@ def avatar_url(context, size=None, user=None):
 @register.simple_tag(takes_context=True)
 def add_active(context, url_name, *args, **kwargs):
     exact_match = kwargs.pop('exact_match', False)
+    not_when = kwargs.pop('not_when', None)
 
     path = reverse(url_name, args=args, kwargs=kwargs)
-    if not exact_match and context.request.path.startswith(path):
+    current_path = context.request.path
+
+    if not_when and str(not_when) in current_path:
+        return ''
+
+    if not exact_match and current_path.startswith(path):
         return ' active '
-    elif exact_match and context.request.path == path:
+    elif exact_match and current_path == path:
         return ' active '
     else:
         return ''
